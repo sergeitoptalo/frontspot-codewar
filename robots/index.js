54f4298ff80c4db3d88b335c47eceae5288c826d
Sergei Toptalo <toptalo@hotmail.com>
2018-07-13 17:56:07 +0300

    robots/feature: animate eyes

--- robots/index.js
+++ robots/index.js
@@ -5,31 +5,42 @@ function HorizontalRobot(robot) {
     this.robotBody = this.robot.querySelector('#robot-body');
     this.awakeningEndpoint = this.robot.querySelector('#robot-body-endpoint');
     this.openEyeEndpoint = document.querySelector('.open-eye-endpoint');
-    // this.leftEyeAnimation = document.querySelector('#left-eye-animation');
+    this.rightEyeAnimation = document.querySelector('#right-eye-animation');
+    this.leftEyeAnimation = document.querySelector('#left-eye-animation');
     this.eyesGroup = document.querySelector('#eyes-group');
     this.pupilsGroup = this.eyesGroup.querySelector('#pupils-group');
     this.leftEye = document.querySelector('#left-eye');
-    this.rightEye = docement.querySelector('#right-eye');
+    this.rightEye = document.querySelector('#right-eye');
     this.awakeningEndpoint.addEventListener('animationend', () => this.awakening());
     this.openEyeEndpoint.addEventListener('animationend', () => this.lookingAround());
     
     this.isSleeping = true;
 
     this.awakening = function () {
-        var animateLeftEye = document.createElementNS('http://www.w3.org/2000/svg','animate');
-        animate.setAttributeNS(null,'attributeName','d');
-        animate.setAttributeNS(null,'from','M70 90 C 74 95, 76 92, 90 89, M90 89 C 76 92, 74 95, 70 90');
-        animate.setAttributeNS(null,'to','M70 90 C 69 72, 91 72, 90 90, M90 90 C 91 107, 69 107, 70 90');
-        animate.setAttribute('dur', '0.2s');
+       /*  var animateLeftEye = document.createElementNS('http://www.w3.org/2000/svg','animate');
+        animateLeftEye.setAttributeNS('http://www.w3.org/1999/xlink','href','#left-eye');
+        animateLeftEye.setAttributeNS(null,'attributeName','d');
+       
+        animateLeftEye.setAttributeNS(null,'from','M70 90 C 74 95, 76 92, 90 89, M90 89 C 76 92, 74 95, 70 90');
+        animateLeftEye.setAttributeNS(null,'to','M70 90 C 69 72, 91 72, 90 90, M90 90 C 91 107, 69 107, 70 90');
+        animateLeftEye.setAttribute('dur', '0.2s');
+
+
 
         var animateRightEye = document.createElementNS('http://www.w3.org/2000/svg','animate');
-        animate.setAttributeNS(null,'attributeName','d');
-        animate.setAttributeNS(null,'from','M44 89 C 58 92, 60 95, 64 90, M64 90 C 60 95, 58 92, 44 89');
-        animate.setAttributeNS(null,'to','M44 90 C 43 72, 65 72, 64 90, M64 90 C 65 107, 43 107, 44 90');
-        animate.setAttribute('dur', '0.2s');
+        animateRightEye.setAttributeNS('http://www.w3.org/1999/xlink','href','#right-eye');
+        animateRightEye.setAttributeNS(null,'attributeName','d');
+        animateRightEye.setAttributeNS(null,'attributeType','XML');
+        animateRightEye.setAttributeNS(null,'from','M70 90 C 74 95, 76 92, 90 89, M90 89 C 76 92, 74 95, 70 90');
+        animateRightEye.setAttributeNS(null,'to','M70 90 C 69 72, 91 72, 90 90, M90 90 C 91 107, 69 107, 70 90');
+        animateRightEye.setAttribute('dur', '0.2s');
+        animateRightEye.setAttribute('fill', 'freeze'); */
 
-        this.leftEye.appendChild(animateLeftEye);
-        this.rightEye.appendChild(animateRightEye);
+
+       // this.leftEye.appendChild(animateLeftEye);
+       // this.eyesGroup.appendChild(animateRightEye);
+       this.rightEyeAnimation.beginElement();
+       this.leftEyeAnimation.beginElement();
         this.pupilsGroup.classList.add('open');
     }
 
